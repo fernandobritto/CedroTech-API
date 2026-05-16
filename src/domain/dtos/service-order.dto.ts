1bcc5575edd520d65e2df3aea25e205f5a6ae7be
Fernando <fernandobritto.tech@gmail.com>
2026-05-16 20:11:59 -0300

    Initial commit

--- src/domain/dtos/service-order.dto.ts
+++
@@ -1,9 +0,0 @@
-export class ServiceOrderDto {
-  id?: string
-  order: string
-  description: string
-  clientCategoryId: string
-  clientOrderId: string
-  status: string
-  flagCedro?: boolean
-}
