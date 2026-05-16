c0da33124645dd0f25352009c1f14b8e99fe60b4
Fernando <fernandobritto.tech@gmail.com>
2026-05-16 20:58:37 -0300

    Initial commit

--- src/@types/queryCommandOutput.d.ts
+++
@@ -1,5 +0,0 @@
-export declare type QueryCommandOutput = Omit<__QueryCommandOutput, 'Items' | 'LastEvaluatedKey'> & {
-  Items?: Record<string, NativeAttributeValue>[]
-
-  LastEvaluatedKey?: Record<string, NativeAttributeValue>
-}
--- src/presenter/health-check-handler.ts
+++
@@ -1,8 +0,0 @@
-import { formatResponse } from '@common/helpers/formatResponse'
-import { type APIGatewayProxyResult } from 'aws-lambda'
-
-export async function handler(): Promise<APIGatewayProxyResult> {
-  return await formatResponse(200, {
-    message: 'OK, the gateway appears to be healthy'
-  })
-}
